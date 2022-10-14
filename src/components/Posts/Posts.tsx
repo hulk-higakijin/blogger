2b7fc47c6a4a9f3abbf97989f3c48d8c875cad3d
hulk-higakijin <hm385.chejptks@gmail.com>
2022-10-14 21:27:16 +0900

    fix: Hydration faliedでたので、aタグやめてLinkコンポーネントで囲った

--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -1,24 +1,27 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import PostTagsComponent from './Tags'
 
 const PostsComponent = ({ post }: { post: Post }) => {
   return (
     <>
-      <a href={post.slug} className="flex flex-col gap-4">
-        <div className="relative w-full h-60">
-          <Image
-            src={post.thumbnail}
-            layout="fill"
-            className="rounded-xl object-cover"
-            alt="thumbnail"
-          />
+      <Link href={post.slug}>
+        <div className="flex flex-col gap-4 cursor-pointer">
+          <div className="relative w-full h-60">
+            <Image
+              src={post.thumbnail}
+              layout="fill"
+              className="rounded-xl object-cover"
+              alt="thumbnail"
+            />
+          </div>
+          <h2 className="text-lg font-semibold">{post.title}</h2>
+          <div className="text-sm text-gray-400 flex flex-col">
+            <PostTagsComponent tags={post.tags} />
+            <p className="ml-auto">{post.date}</p>
+          </div>{' '}
         </div>
-        <h2 className="text-lg font-semibold">{post.title}</h2>
-        <div className="text-sm text-gray-400 flex flex-col">
-          <PostTagsComponent tags={post.tags} />
-          <p className="ml-auto">{post.date}</p>
-        </div>
-      </a>
+      </Link>
     </>
   )
 }
