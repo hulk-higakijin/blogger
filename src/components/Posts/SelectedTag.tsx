eb6ebe3a7973c4386fed73e7888709db25e153ee
hulk-higakijin <hm385.chejptks@gmail.com>
2022-11-06 22:03:56 +0900

    style: selectedTagのスタイルがバグったので、一旦コメントアウト

--- src/components/Posts/SelectedTag.tsx
+++ src/components/Posts/SelectedTag.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 const SelectedTagComponent = ({ tag }: { tag: string }) => {
   return (
     <>
-      <div className="flex gap-2 border w-fit px-2 bg-gray-100 rounded-full text-gray-600">
+      {/* <div className="flex gap-2 border w-fit px-2 bg-gray-100 rounded-full text-gray-600 ">
         <FontAwesomeIcon icon={faTag} className="my-auto" />
         <p className="font-bold">{tag}</p>
         <Link href="/">
@@ -14,7 +14,7 @@ const SelectedTagComponent = ({ tag }: { tag: string }) => {
             className="my-auto text-sm text-gray-600 hover:text-red-500 hover:cursor-pointer pl-1"
           />
         </Link>
-      </div>
+      </div> */}
     </>
   )
 }
